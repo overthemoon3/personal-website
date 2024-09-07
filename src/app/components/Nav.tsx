57a59545c2b7a93956e93c3fdf96400226a98b9d
sak-kon-ucsc <108996976+sak-kon-ucsc@users.noreply.github.com>
2024-09-07 14:44:48 -0700

    updated a little but going to change big shit soon

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -6,18 +6,35 @@ import './nav.css'
 
 export default function Nav() {
     const [navList, setNavList] = useState(navs)
+
+    const handleClick = (id: number, e: React.MouseEvent) => {
+        e.preventDefault();
+
+        // toggle expansion items only 
+        setNavList(navList.map(nav =>
+            nav.id === id ? { ...nav, active: !nav.active} : { ...nav, active: false}
+        ));
+    };
+
+
     return (
         <nav id='navbar' className='navbar nav-menu'>
             <ul>
                 {
                     navList.map(nav=>(
                         <li>
-                            <a className={`nav-link scrollto ${
-                                nav.active ? 'active' : undefined
-                                }`}
+                            <a 
+                                href={nav.target === 'linkedin' ? 'www.linkedin.com/in/sakshi-konnur752' : nav.target === 'github' ? 'https://github.com/sak-kon-ucsc' : '#'}
+                                className = {`nav-link ${nav.active ? 'active' : ''} ${nav.target === 'linkedin' || nav.target === 'github' ? 'no-expand' : 'expandable'}`}
+                                onClick={(e) => handleClick(nav.id, e)}
+                                target={nav.target === 'linkedin' || nav.target === 'github' ? '_blank' : undefined}
+                                rel={nav.target === 'linkedin' || nav.target === 'github' ? 'noopener noreferrer' : undefined}
+                            // className={`nav-link scrollto ${
+                            //     nav.active ? 'active' : undefined
+                            //     }`}
                             >
                                 <i className={nav.icon}></i>
-                                <span>{nav.name}</span>
+                                {nav.name && <span>{nav.name}</span>}
                             </a>
                         </li>
                     ))
--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -8,20 +8,20 @@ export const navs = [
         icon: 'bx bx-home',
         active: true,
     },
-    {
-        id: 2, 
-        name: 'About',
-        target: 'about',
-        icon: 'bx bx-user',
-        active: false,
-    }, 
-    {
-        id: 3,
-        name: 'Work In Progress',
-        target: 'wip',
-        icon: 'bi bi-tools',
-        active: false
-    },
+    //{
+    //    id: 2, 
+    //   name: 'About',
+    //   target: 'about',
+    //    icon: 'bx bx-user',
+    //    active: false,
+    //}, 
+    //{
+    //    id: 3,
+    //    name: 'Work In Progress',
+    //    target: 'wip',
+    //    icon: 'bi bi-tools',
+    //    active: false
+    //},
     {
         id: 4,
         target: 'linkedin',
