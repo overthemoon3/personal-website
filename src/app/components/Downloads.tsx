7991d85dd8862f813805d2a1ea0748fb677c85db
sak-kon-ucsc <108996976+sak-kon-ucsc@users.noreply.github.com>
2024-09-12 13:01:48 -0700

    got resume pdf uploaded and resume link and icon works as intended

--- src/app/components/Downloads.tsx
+++ src/app/components/Downloads.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Downloads.module.css';
+import { FiDownload } from "react-icons/fi";
 
 const Downloads: React.FC = () => {
     return (
         <section className={styles.downloads}>
-            <a href="/resume.pdf" download className={styles.link}>Resume</a>
+            <a href="/resume0.pdf" download className={styles.link}>
+                Resume <FiDownload className={styles.icon}/> 
+            </a>
             { /*<a href="/transcript.pdf" download className={styles.link}>Transcript</a> */}
         </section>
     );
