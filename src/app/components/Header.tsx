9831486874c18fda814c4af4972ee8cf793ec79d
sak-kon-ucsc <108996976+sak-kon-ucsc@users.noreply.github.com>
2024-09-11 18:33:29 -0700

    figured out alignment

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,17 +4,20 @@
 
 // functional component: 
 import React from 'react';
-import './Header.module.css'
+import styles from './Header.module.css';
 import Nav from './Nav'
 
 const Header: React.FC = () => {
     return (
-        <header>
-            <Nav />
-            <section>
-            <p>I'm Sakshi, a third year Computer Science student at the University of California - Santa Cruz. I work great in teams and can be counted on to consistently deliver above and beyond.</p>
-            </section>            
-        </header>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <section className={styles.section}>
+                    <p>I'm Sakshi, a third year Computer Science student at the University of California - Santa Cruz. I work great in teams and can be counted on to consistently deliver above and beyond.</p>
+                </section>  
+                <Nav />   
+            </div>
+                   
+        </header> 
     );
 };
 
